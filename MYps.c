9fc68298298546a464c7124f74712fe3405ad2cc
tun66280 <tun66280>
2023-09-28 20:30:03 -0400

    week 1 complete

--- MYps.c
+++ MYps.c
@@ -7,6 +7,8 @@
 #include <sys/stat.h>
 #include <limits.h> 
 #include <unistd.h>
+#include <fcntl.h>
+#define FULL_PATH 256
 typedef struct{
 int p;
 int s;
@@ -15,6 +17,16 @@ int S;
 int v;
 int c;
 }options;
+
+
+
+typedef struct{
+int pid;
+char state;
+unsigned int utime;
+unsigned vtime;
+
+}ProcessMetadata;
 
 void DisplayOptions(options Myoptions){
     printf("Option p is:%d\n",Myoptions.p);
@@ -27,6 +39,108 @@ void DisplayOptions(options Myoptions){
 
 
 }
+
+int set_Proc_Metadata(ProcessMetadata *Proc){};
+
+
+
+
+
+int isDir(char *path){
+
+struct stat stbuf;
+
+if( stat(path,&stbuf)==0){
+   
+   if( S_ISDIR(stbuf.st_mode)){
+    return 1;
+
+   }
+
+   else{return 0;}
+
+}
+
+else{
+
+    perror("bad path" );
+
+    return 0;
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+char  *parseStat_Statm(char *path,int field){
+ static char line[FULL_PATH];
+FILE *stream;
+if((stream=fopen(path,"r"))==NULL){
+    perror("stream");
+    exit(1);
+}
+
+fgets(line,sizeof(line),stream);
+printf("%s\n",line);
+int count=0;
+char *delim=" ";
+char *token=strtok(line, delim);
+
+    printf("\n\n\nhere we are %s\n\n",token);
+    
+
+while(token!=NULL ){
+    if(count==field-1){
+    char *target=token;
+    fclose(stream);
+    printf(" found target %s !!!", target);
+    return target;
+    }
+    count++;
+
+    token=strtok(NULL, delim);
+
+        printf("\n\n\nhere we are %s\n\n",token);
+
+}
+
+    printf("%s","field not present");
+    fclose(stream);
+    return NULL;
+
+}
+
+
+
+
+void  parseCMD_Line(char *path){
+static char line[FULL_PATH];
+FILE *stream;
+if((stream=fopen(path,"r"))==NULL){
+    perror("stream");
+    exit(1);
+}
+
+fgets(line,sizeof(line),stream);
+printf("%s\n",line);
+    
+}
+
+
+
+
+
+
+
+
 
 options defaultConstructor(){
 options OptionConstructor;    
@@ -44,6 +158,10 @@ return OptionConstructor;
 
 
 int main(int argc, char **argv){
+    int utime_Field;
+    int PID_Field;
+    int state_Field;
+    int stime_Field;
 
     options optionStructure=defaultConstructor();
 
@@ -82,11 +200,36 @@ int main(int argc, char **argv){
 
 
 }
+parseCMD_Line("/proc/1/cmdline");
 
+return 0;
+} 
 
-DisplayOptions(optionStructure);
+/*
+char * var= parseStat_Statm("/proc/1/stat",2);
+printf(" i got you %s ",var);
 
-    return 0;
+return 0;
+} 
+
+if(isDir("/proc/1071374")==1){
+
+    printf("%s\n", "is a Directory");
 }
+
+else{
+
+    printf("Nope");
+}
+
+//DisplayOptions(optionStructure);
+*/
+
+
+
+
+// Open the file using the open function
+
+
 
 
