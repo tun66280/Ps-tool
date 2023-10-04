564a6b8bbb6e846099ba3d2a00e3e2a17e6145ac
tun66280 <tun66280>
2023-10-04 15:55:34 -0400

    AMY IM ALMOST DONE./ps -s -v./ps -s -v

--- MYps.c
+++ MYps.c
@@ -9,6 +9,76 @@
 #include <unistd.h>
 #include <fcntl.h>
 #define FULL_PATH 1048
+int pathNonExistant(char *path){
+struct stat stbuf;
+
+if(stat(path,&stbuf)!=0){
+
+perror(path);
+
+
+}
+
+
+}
+
+
+
+char *parseCMD_Line(char *path){
+static char line[FULL_PATH];
+FILE *stream;
+if((stream=fopen(path,"r"))==NULL){
+    perror("CMD");
+    exit(1);
+}
+
+fgets(line,sizeof(line),stream);
+//printf("%s\n",line);
+return line;
+    
+}
+
+
+
+
+char  *parseStat_Statm(char *path,int field){
+ static char line[FULL_PATH];
+FILE *stream;
+if((stream=fopen(path,"r"))==NULL){
+    perror(path);
+    exit(1);
+}
+
+fgets(line,sizeof(line),stream);
+//printf("%s\n",line);
+int count=0;
+char *delim=" ";
+char *token=strtok(line, delim);
+
+   // printf("\n\n\nhere we are %s\n\n",token);
+    
+
+while(token!=NULL ){
+    if(count==field-1){
+    char *target=token;
+    fclose(stream);
+   // printf(" found target %s !!!", target);
+    return target;
+    }
+    count++;
+
+    token=strtok(NULL, delim);
+
+      //  printf("\n\n\nhere we are %s\n\n",token);
+
+}
+
+   // printf("%s","field not present");
+    fclose(stream);
+    return NULL;
+
+}
+
 typedef struct{
 int p;
 int s;
@@ -22,11 +92,51 @@ int c;
 
 typedef struct{
 int pid;
-char state;
-unsigned int utime;
-unsigned vtime;
+char *state;
+ int utime;
+int stime;
+int virtualMem;
+char *cmdline;
+
 
 }ProcessMetadata;
+
+
+ProcessMetadata MetadataConstructor(){
+ProcessMetadata MetadataConstructor;    
+MetadataConstructor.pid=-1;
+MetadataConstructor.state=NULL;
+MetadataConstructor.utime=-1;
+MetadataConstructor.stime=-1;
+MetadataConstructor.virtualMem=-1;
+MetadataConstructor.cmdline=NULL;
+
+
+
+
+
+
+
+
+return MetadataConstructor;
+
+    
+}
+
+
+options defaultConstructor(){
+options OptionConstructor;    
+OptionConstructor.p=-1;
+OptionConstructor.s=-1;
+OptionConstructor.U=-1;
+OptionConstructor.s=-1;
+OptionConstructor.v=-1;
+OptionConstructor.c=-1;
+
+return OptionConstructor;
+
+    
+}
 
 void DisplayOptions(options Myoptions){
     printf("Option p is:%d\n",Myoptions.p);
@@ -40,7 +150,104 @@ void DisplayOptions(options Myoptions){
 
 }
 
-int set_Proc_Metadata(ProcessMetadata *Proc){};
+
+void DisplayProcInfo(ProcessMetadata *Process, options *Myoptions){
+         //   printf("%s","ANIM OHEN!!!!!!!!");
+
+
+char procpath[FULL_PATH];
+snprintf(procpath,sizeof(procpath),"%s%d","/proc/",Process->pid);
+if(pathNonExistant(procpath)==1){
+
+    exit(1);
+
+}
+    
+    printf("%d: ",Process->pid);
+
+    if(Myoptions->s==1){
+
+        printf("%s ",Process->state);
+    }
+
+    if(Myoptions->U!=1){
+        
+        printf("utime=%d ",Process->utime);
+    }
+
+    if(Myoptions->S==1){
+
+        printf("stime:%d ",Process->stime);
+    }
+
+     if(Myoptions->v==1){
+
+        printf("virtual Memory:%d ",Process->virtualMem);
+    }
+
+
+    if(Myoptions->c!=1){
+
+        printf("%s", Process->cmdline);
+    }
+
+    printf("%s","\n\n");
+
+
+
+}
+
+int set_Proc_Metadata(char *Pid, options *optionStructure,  ProcessMetadata *Proc, int recursion){
+//this function initialzies process field ino ProcessMetadata struct components
+
+
+if(recursion==1){ // manually set PID
+    if(optionStructure->p!=1){
+    Proc->pid=atoi(Pid);
+}
+}
+
+if(optionStructure->s==1){
+    char absPath[FULL_PATH];
+    snprintf(absPath,sizeof(absPath),"%s%d%s","/proc/",Proc->pid,"/stat");
+    
+   // printf("ANIMMMQQQWeare:%s\n\n\n",absPath);
+    printf("%s\n"," State DEBUG");
+    Proc->state=parseStat_Statm(absPath,3);
+    printf("State is DEBUG:%s\n",Proc->state);
+
+}
+
+if(optionStructure->U!=1){
+
+    char absPath[FULL_PATH];
+    snprintf(absPath,sizeof(absPath),"%s%d%s","/proc/",Proc->pid,"/stat");
+    Proc->utime=  atoi( parseStat_Statm(absPath,14));   
+}
+
+
+if(optionStructure->S==1){
+    char absPath[FULL_PATH];
+    snprintf(absPath,sizeof(absPath),"%s%d%s","/proc/",Proc->pid,"/stat");
+    Proc->stime= atoi( parseStat_Statm(absPath,15));
+}
+
+
+if(optionStructure->v==1){
+    char absPath[FULL_PATH];
+    snprintf(absPath,sizeof(absPath),"%s%d%s","/proc/",Proc->pid,"/statm");
+    Proc->virtualMem= atoi( parseStat_Statm(absPath,1));
+}
+
+if(optionStructure->c!=1){
+
+     char absPath[FULL_PATH];
+    snprintf(absPath,sizeof(absPath),"%s%d%s","/proc/",Proc->pid,"/cmdline");
+    Proc->cmdline=parseStat_Statm(absPath,1);
+}
+
+}
+
 
 uid_t uidcmp(char *path){
 
@@ -108,82 +315,35 @@ else{
 
 
 
-int isDir(char *path){
 
-struct stat stbuf;
 
-if( stat(path,&stbuf)==0){
-   
-   if( S_ISDIR(stbuf.st_mode)){
-    return 1;
 
-   }
 
-   else{return 0;}
 
+
+
+
+
+
+
+
+
+
+void dipslayProc(char *Pid, options *optionStructure,  ProcessMetadata *Proc, int recursion){
+if(recursion==0){
+    printf("%s\n\n"," DEBUG 1 WEEEE AREEEE!!!");
+
+set_Proc_Metadata("default", optionStructure, Proc,  recursion);
+
+printf("%s\n\n","DEBUG 2 AND NOW WEEEE AREEEE!!!");
+
+DisplayProcInfo( Proc,  optionStructure);
 }
+
 
 else{
 
-    perror("bad path" );
 
-    return 0;
-}
-
-}
-
-
-
-
-
-
-
-
-
-
-
-char  *parseStat_Statm(char *path,int field){
- static char line[FULL_PATH];
-FILE *stream;
-if((stream=fopen(path,"r"))==NULL){
-    perror("stream");
-    exit(1);
-}
-
-fgets(line,sizeof(line),stream);
-printf("%s\n",line);
-int count=0;
-char *delim=" ";
-char *token=strtok(line, delim);
-
-    printf("\n\n\nhere we are %s\n\n",token);
-    
-
-while(token!=NULL ){
-    if(count==field-1){
-    char *target=token;
-    fclose(stream);
-    printf(" found target %s !!!", target);
-    return target;
-    }
-    count++;
-
-    token=strtok(NULL, delim);
-
-        printf("\n\n\nhere we are %s\n\n",token);
-
-}
-
-    printf("%s","field not present");
-    fclose(stream);
-    return NULL;
-
-}
-
-
-
-
-void listProcDirectory(){
 
 DIR *stream;
 struct dirent **namelist;
@@ -199,8 +359,14 @@ if(n==-1){
 }
 
 for (int i=0; i<n;i++){
-    if(strcmp(namelist[i]->d_name,".")!=0 && strcmp(namelist[i]->d_name,"..")!=0 ){
-    printf("%s\n",namelist[i]->d_name);
+    if(strcmp(namelist[i]->d_name,"1")==0 || strcmp(namelist[i]->d_name,"10")==0 ){
+    
+//printf("For the others");
+
+    set_Proc_Metadata(namelist[i]->d_name,  optionStructure,   Proc,  recursion);
+    DisplayProcInfo( Proc,  optionStructure);
+
+    *Proc=MetadataConstructor();
     free(namelist[i]);
     }
 }
@@ -209,6 +375,7 @@ free(namelist);
 
 
 
+}
 
 
 }
@@ -216,57 +383,41 @@ free(namelist);
 
 
 
-void  parseCMD_Line(char *path){
-static char line[FULL_PATH];
-FILE *stream;
-if((stream=fopen(path,"r"))==NULL){
-    perror("stream");
-    exit(1);
-}
-
-fgets(line,sizeof(line),stream);
-printf("%s\n",line);
-    
-}
-
-
-
-
-
-
-
-
-
-options defaultConstructor(){
-options OptionConstructor;    
-OptionConstructor.p=0;
-OptionConstructor.s=0;
-OptionConstructor.U=0;
-OptionConstructor.s=0;
-OptionConstructor.v=0;
-OptionConstructor.c=0;
-
-return OptionConstructor;
-
-    
-}
 
 
 int main(int argc, char **argv){
-    int utime_Field;
-    int PID_Field;
-    int state_Field;
-    int stime_Field;
+
+     int recursion=1;
+    
+
+
 
     options optionStructure=defaultConstructor();
 
+    ProcessMetadata procMetadata=MetadataConstructor();
 
-    int options;
 
-    while((options=getopt(argc,argv,"psUSvc"))!=-1){    
+    int options=0;
+    int value=0;
+    char *endcharValint;
+
+    while((options=getopt(argc,argv,"p:sUSvc"))!=-1){    
         switch(options){
         case 'p':
             optionStructure.p=1;
+            recursion=0;
+
+            value=strtol(optarg,&endcharValint,10);
+
+            if(*endcharValint=='\0'){
+                procMetadata.pid=value;
+              //  printf("THEREEEEEEE:%d\n",procMetadata.pid);
+            }
+
+            else{
+                perror("conversion");
+                exit(1);
+            }
             break;
         case 's':
             optionStructure.s=1;
@@ -295,13 +446,37 @@ int main(int argc, char **argv){
 
 
 }
-//parseCMD_Line("/proc/1391801/cmdline");
 
-//parseStat_Statm("/proc/1391801/stat",3);
 
-//listProcDirectory();
-printf("%d\n",procUIDcmp("/proc/10"));
+printf("%s\n\n","ANIM IT WORKSSSSS");
+
+dipslayProc("default",   &optionStructure,   &procMetadata,  recursion);
+
 
 
 return 0;
 } 
+/*int isDir(char *path){
+
+struct stat stbuf;
+
+if( stat(path,&stbuf)==0){
+   
+   if( S_ISDIR(stbuf.st_mode)){
+    return 1;
+
+   }
+
+   else{return 0;}
+
+}
+
+else{
+
+    perror("bad path" );
+
+    return 0;
+}
+
+} */
+
